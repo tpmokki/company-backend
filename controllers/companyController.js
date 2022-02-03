3273496867e198362f430d72cc06584e57adee59
Teemu Pesonen <musta.pippuri@gmail.com>
2022-02-03 09:40:43 +0200

    Company data can be formatted to the specified structure.

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -7,14 +7,47 @@ const getByBusinessId = async (req, res) => {
   try {
     const response = await axios.get(url)
     let rawData = response.data.results
+    let companyData = {}
 
-    res.status(200).json(rawData);
+    if (rawData.length > 0) {
+      companyData = companyObjectFromRawData(rawData[0])
+    } else {
+      res.status(404).json({ error: 'Not Found' })
+    }
+    
+    res.status(200).json(companyData);
   } catch (err) {
     let errorStatus = err.response.status
     let errorText = err.response.statusText
 
-    res.status(errorStatus).json({ error: errorText });
+    res.status(errorStatus).json({ error: errorText })
   }
+}
+
+const companyObjectFromRawData = (data) => {
+  let business_id = data.businessId
+  let name = data.name
+  let addressObject = data.addresses.find(a => !a.endDate && a.type === 2)// 'type': '2' means postal address
+  let address = addressStringFromObject(addressObject)
+  let phone = data.contactDetails.find(c => !c.endDate && c.type === 'Mobile phone').value
+  let website = data.contactDetails.find(c => !c.endDate && c.type === 'Website address').value
+  
+  return {
+    business_id,
+    name,
+    address,
+    phone,
+    website
+  }
+}
+
+const addressStringFromObject = (addressObj) => {
+  let street = addressObj.street
+  let postCode = addressObj.postCode || ''
+  let city = addressObj.city
+  city = addressObj.city.charAt(0) + addressObj.city.slice(1).toLowerCase()
+
+  return `${street}, ${postCode} ${city}`
 }
 
 module.exports = { getByBusinessId }
